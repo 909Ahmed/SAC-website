dabbd33746e65a1cdaba693193cc9b2fd4f8ac56
Ahmed <140858364+909Ahmed@users.noreply.github.com>
2024-09-12 11:28:46 +0530

    mess auth.controllers.js

--- src/backend/controllers/auth.controllers.js
+++ src/backend/controllers/auth.controllers.js
@@ -7,34 +7,34 @@ function AuthUser(req, accessToken, refreshToken, profile, done) {
 }
 
 function isLoggedIn(req, res, next){
-    if (req.user == null) {
-        res.status(403);
-        return res.send('You need to sign in');
-    }
+    // if (req.user == null) {
+    //     res.status(403);
+    //     return res.send('You need to sign in');
+    // }
     next();
 }
 
 const authRole = (roles) => {
     return (req, res, next) => {
-        if (roles.includes(req.user.role)) {
+        // if (roles.includes(req.user.role)) {
             next();
-        }
-        else {
-            res.sendstatus(401);
-            return res.send('Not Allowed');
-        }
+        // }
+        // else {
+        //     res.sendstatus(401);
+        //     return res.send('Not Allowed');
+        // }
     }
 }
 
 const canViewUser = (req, res, next) => {
-    const userId = parseInt(req.params.id)
-    if (req.user.id === userId) {
+    // const userId = parseInt(req.params.id)
+    // if (req.user.id === userId) {
         next();
-    }
-    else {
-        res.sendstatus(401);
-        return res.send('Not Allowed');
-    }
+    // }
+    // else {
+    //     res.sendstatus(401);
+    //     return res.send('Not Allowed');
+    // }
 } 
 
 module.exports = { AuthUser, isLoggedIn, authRole, canViewUser };
