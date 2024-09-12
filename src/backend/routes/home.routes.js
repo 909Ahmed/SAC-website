3aa8f67b8dd83bed55ec49da8b6e51f80b594ce6
Ahmed <140858364+909Ahmed@users.noreply.github.com>
2024-09-12 11:31:04 +0530

    delete auth
    yes I changed em in github

--- src/backend/routes/home.routes.js
+++ src/backend/routes/home.routes.js
@@ -6,13 +6,13 @@ const userRoutes = require("./users.routes");
 const threadRoutes = require("./threads.routes");
 const announcementRoutes = require("./announcements.routes");
 const clubRoutes = require("./clubs.routes");
-const auth = require("./auth.routes");
+// const auth = require("./auth.routes");
 
 // Use the routers
 router.use("/users", userRoutes);
 router.use("/threads", threadRoutes);
 router.use("/announcements", announcementRoutes);
 router.use("/clubs", clubRoutes);
-router.use("/auth", auth);
+// router.use("/auth", auth);
 
 module.exports = router;
