8b01fd85613c6ce8b5c56c38b207033fd550de9e
909hehe <crypticahmed@gmail.com>
2024-09-14 21:30:31 +0530

    nothings happened

--- src/backend/models/database.js
+++ src/backend/models/database.js
@@ -7,7 +7,6 @@ const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   // host: POSTGRES_HOST,
   dialect: "postgres",
   port: 5432,
-  // POSTGRES_URL="postgres://default:lxytp2Y3sveq@ep-proud-cell-a4pund3o-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
   sslmode: "require"
 });
 
